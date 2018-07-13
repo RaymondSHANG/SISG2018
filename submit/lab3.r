309718cae71923291d64fc2ce109ae5590f48363
SISG2018 <40276124+SISG2018@users.noreply.github.com>
2018-07-13 11:32:05 -0700

    Update lab3.r

--- submit/lab3.r
+++ submit/lab3.r
@@ -1,3 +1,23 @@
+library(gee)
+library(lmtest)
+library(multcomp)
+library(dplyr)
+library(ggplot2)
+### Load data
+cholesterol = read.csv("https://raw.githubusercontent.com/rhubb/SISG2018/master/data/SISG-Data-cholesterol.csv", header=T)
+
+### Simple Linear Regression
+
+str(cholesterol)
+head(cholesterol)
+
+# attach(cholesterol)
+
+
+
+table(cholesterol$rs4775401, cholesterol$APOE)
+
+
 means <- aggregate(chol ~  as.factor(rs4775401), cholesterol, mean)
 ggplot(cholesterol, aes(x = as.factor(rs4775401),
                         y = chol, 
@@ -14,3 +34,23 @@ ggplot(cholesterol, aes(x = as.factor(APOE),
                         fill = as.factor(APOE))) + geom_boxplot() + 
    stat_summary(fun.y=mean, colour="darkred", geom="point", 
                shape=18, size=3)
+
+
+
+ggplot(cholesterol, aes(x = as.factor(APOE),
+                        y = chol, 
+                        fill = as.factor(rs4775401))) + 
+   geom_boxplot() 
+
+
+# ANCOVA including age and SNP
+fit1 = lm(chol ~ factor(rs4775401) + factor(APOE))
+summary(fit1)
+
+
+fit2 = lm(chol ~ factor(rs4775401) * factor(APOE))
+summary(fit2)
+
+anova(fit1, fit2)
+
+
