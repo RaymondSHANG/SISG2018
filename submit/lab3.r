9b61af1c0ecdd5e7b826c6b088ffa6dd96686906
SISG2018 <40276124+SISG2018@users.noreply.github.com>
2018-07-13 11:22:05 -0700

    Update lab3.r

--- submit/lab3.r
+++ submit/lab3.r
@@ -22,17 +22,23 @@ means <- aggregate(chol ~  as.factor(rs4775401), cholesterol, mean)
 ggplot(cholesterol, aes(x = as.factor(rs4775401),
                         y = chol, 
                         color = as.factor(rs4775401))) + 
-   geom_boxplot() 
+   geom_boxplot() + 
 
-   +stat_summary(fun.y=mean, colour="darkred", geom="point", 
-               shape=18, size=3) + 
-  geom_text(data = means, aes(label = chol, y = chol + 0.08))
+   stat_summary(fun.y=mean, colour="darkred", geom="point", 
+               shape=18, size=3) 
+   # + 
+  # geom_text(data = means, aes( label = chol, y = chol + 0.08))
 
-
+means <- aggregate(chol ~  as.factor(APOE), cholesterol, mean)
 ggplot(cholesterol, aes(x = as.factor(APOE),
                         y = chol, 
                         color = as.factor(APOE))) + 
-   geom_boxplot() 
+   geom_boxplot() + 
+
+   stat_summary(fun.y=mean, colour="darkred", geom="point", 
+               shape=18, size=3)
+
+
 
 ggplot(cholesterol, aes(x = as.factor(APOE),
                         y = chol, 
