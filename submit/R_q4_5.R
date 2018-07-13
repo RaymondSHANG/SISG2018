c49a2764a91a4ce117e9847b89709c19a38526d1
SISG2018 <40276124+SISG2018@users.noreply.github.com>
2018-07-13 16:23:00 -0700

    Add files via upload

---
+++ submit/R_q4_5.R
@@ -0,0 +1,27 @@
+## Use a GLM to estimate the relative risk of hypertension for patients with different rs174548 genotypes, 
+## adjusting for triglyceries. Make sure you can interpret the coefficients. 
+#How do these results compare to the results of the logistic regression analysis?
+
+# Load lmtest library for likelihood ratio tests
+library(lmtest)
+
+### Relative risk regression
+glm.rr <- gee(HTN ~ TG+factor(rs174548), family = "poisson", id = seq(1,nrow(cholesterol)), data = cholesterol)
+summary(glm.rr)
+exp(glm.rr$coef)
+
+#Compare with results from logistic reg
+exp(glm.mod3$coef)
+
+#5. Use a GLM to estimate the risk difference for hypertension according to rs174548 genotypes, 
+#adjusting for triglyceries. Make sure you can interpret the coef=cients. 
+#How do these results compare to the results of the logistic regression and relative risk regression analyses?
+
+### Risk difference regression
+glm.rd <- gee(HTN ~ TG+factor(rs174548), id = seq(1,nrow(cholesterol)), data = cholesterol)
+summary(glm.rd)
+
+#Compare with results from logistic reg
+exp(glm.mod3$coef)
+#compare with results from relative risk reg
+exp(glm.rr$coef)
