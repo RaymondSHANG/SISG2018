2055ac6896c7ee82e607f75bd5e6247fd13469a6
SISG2018 <40276124+SISG2018@users.noreply.github.com>
2018-07-13 16:09:07 -0700

    Add files via upload

--- submit/Logistic Regression Lab4.R
+++ submit/Logistic Regression Lab4.R
@@ -4,7 +4,7 @@ par(mfrow=c(1,1))
 table(factor(HTN), factor(rs174548))
 fisher.test(factor(HTN), factor(rs174548))
 
-htnsnplr<-glm(HTN~rs174548, family=binomial)
+htnsnplr<-glm(HTN~factor(rs174548), family=binomial)
 summary(htnsnplr)
 exp(htnsnplr$coef)
 exp(confint(htnsnplr))
@@ -16,9 +16,17 @@ exp(10*htntglr$coef) # this gives me the odds ratio for getting hypertension for
 exp(confint(htntglr))
 
 # Association between HTN and rs174548 adjusted for TG, what role does TG play
-htntgsnp<-glm(HTN~rs174548+TG, family=binomial)
+htntgsnp<-glm(HTN~factor(rs174548)+TG, family=binomial)
 summary(htntgsnp)
 # compare htntgsnp model to htnsnp model to see if adding tg is significant/important so use likelihood ratio test
 lrtest(htnsnplr, htntgsnp)  # based on o utput where lrtest p value < 0.05, after accounting for snp, there is a significant association between tg and HTN
 
+# now look at relative risk model where u use gee (glm w poisson distribution but using gee b/c variances not equal)
+relrisk<-gee(HTN~factor(rs174548)+TG, family=poisson, id=seq(1,nrow(cholesterol), data=cholesterol))
+summary(relrisk) # since it's a log link it gives  u a log rel risk so still exponentiate it
+exp(relrisk$coef)
 
+# now look at risk difference model where u use gee (glm w normal distribution b/c but using gee b/c fitting a linear model for binary data)
+
+riskdif <-gee(HTN~factor(rs174548)+TG, id=seq(1,nrow(cholesterol), data=cholesterol)) # if u dont specify family, default is normal
+summary(riskdif)
